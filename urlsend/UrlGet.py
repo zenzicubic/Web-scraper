79e53dfdbefd1b3e94785d1cfd1584a549864f7e
Jacob-Python <58337545+Jacob-Python@users.noreply.github.com>
2019-11-29 18:29:32 -0500

    Update UrlGet.py

--- urlsend/UrlGet.py
+++ urlsend/UrlGet.py
@@ -1,7 +1,7 @@
 import urllib.request
 
 # Reading the URL data and writing it to a file.
-UrlDat = urllib.request.urlopen("https://www.reddit.com/r/Ooer")
+UrlDat = urllib.request.urlopen("https://url")
 file = open("data.txt", "w+")
 file.write(str(UrlDat.read()))
 file.close()
